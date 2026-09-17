011d855f78bede9930681dd4f6476291a4542685
ปฏิภาณ ใจมั่น <cimanptiphan@gmail.com>
2026-09-17 19:03:39 +0700

    Add files via upload

---
+++ point-of-Sate_ep2_CreateDatabase/src/main/java/com/pos/PointOfSateApplication.java
@@ -0,0 +1,13 @@
+package com.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSateApplication.class, args);
+	}
+
+}
